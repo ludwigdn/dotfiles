ffa7717fc0e9bbab535d415e504157c2323d04de
ludwigdn <11355187+ludwigdn@users.noreply.github.com>
2022-01-13 15:21:37 +0100

    update setup sh

--- symbolic-links-setup.sh
+++ symbolic-links-setup.sh
@@ -1,7 +1,9 @@
 # .vimrc -------------------------------
-rm ~/.vimrc
-ln -s ~/git/dotvim/.vimrc ~/.vimrc
+echo 'Adding .vimrc' \
+&& rm ~/.vimrc > /dev/null 2>&1 \
+&& ln -s ~/git/dotvim/.vimrc ~/.vimrc
 
 # splitted vim files -------------------
-rm ~/.vimsplit
-ln -s ~/git/dotvim/.vimsplit ~/.vimsplit
+echo 'Adding vimsplit folder' \
+&& rm ~/.vimsplit > /dev/null 2>&1 \
+&& ln -s ~/git/dotvim/.vimsplit ~/.vimsplit
