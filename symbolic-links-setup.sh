295ced005e3d1ff91ae25627c982fa4b82ba7c29
ludwigdn <11355187+ludwigdn@users.noreply.github.com>
2022-06-02 11:51:36 +0200

    Fix absolute path in symlink setup file

--- symbolic-links-setup.sh
+++ symbolic-links-setup.sh
@@ -1,9 +1,9 @@
 # .vimrc -------------------------------
 echo 'Adding .vimrc' \
 && rm ~/.vimrc > /dev/null 2>&1 \
-&& ln -s .vimrc ~/.vimrc
+&& ln -s $CODE_HOME/dotvim/.vimrc ~/.vimrc
 
 # splitted vim files -------------------
 echo 'Adding vimsplit folder' \
 && rm ~/.vimsplit > /dev/null 2>&1 \
-&& ln -s  .vimsplit ~/.vimsplit
+&& ln -s $CODE_HOME/dotvim/.vimsplit ~/.vimsplit
