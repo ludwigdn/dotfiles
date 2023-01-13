5fb569df74daf083066c2f7e64eef4fa46f8c6c3
ludwigdn <11355187+ludwigdn@users.noreply.github.com>
2023-01-13 11:52:19 +0100

    update lualine config

--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -1,6 +1,19 @@
 local setup, lualine = pcall(require, "lualine")
 if not setup then
-  return
+	return
 end
 
-lualine.setup()
+lualine.setup({
+	sections = {
+		lualine_c = { { "filename", path = 1 } },
+		lualine_x = { "filetype" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { { "filename", path = 1 } },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+})
