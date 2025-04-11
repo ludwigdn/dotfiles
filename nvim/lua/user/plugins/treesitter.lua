a8ee88ec26a33e7c5fc33807050fe156e3681ed3
ludwigdn <11355187+ludwigdn@users.noreply.github.com>
2025-04-11 11:01:34 +0200

    treesitter: add typescript

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,44 +1,45 @@
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+  return
 end
 
 -- configure treesitter
 treesitter.setup({
 
-	-- enable syntax highlighting
-	highlight = {
-		enable = true,
-	},
+  -- enable syntax highlighting
+  highlight = {
+    enable = true,
+  },
 
-	-- enable indentation
-	indent = { enable = true },
+  -- enable indentation
+  indent = { enable = true },
 
-	-- enable autotagging (w/ nvim-ts-autotag plugin)
-	autotag = { enable = true },
+  -- enable autotagging (w/ nvim-ts-autotag plugin)
+  autotag = { enable = true },
 
-	-- ensure these language parsers are installed
-	ensure_installed = {
-		"html",
-		"css",
-		"scss",
-		"javascript",
-		"tsx",
-		"vue",
+  -- ensure these language parsers are installed
+  ensure_installed = {
+    "html",
+    "css",
+    "scss",
+    "javascript",
+    "typescript",
+    "tsx",
+    "vue",
 
-		"bash",
+    "bash",
 
-		"json",
-		"markdown",
-		"markdown_inline",
-		"mermaid",
-		"dockerfile",
-		"yaml",
+    "json",
+    "markdown",
+    "markdown_inline",
+    "mermaid",
+    "dockerfile",
+    "yaml",
 
-		"lua",
-		"vim",
-	},
+    "lua",
+    "vim",
+  },
 
-	-- auto install above language parsers
-	auto_install = true,
+  -- auto install above language parsers
+  auto_install = true,
 })
