cd2a2cd0deba2307ad03f22d056f6f9d31004d56
ludwigdn <11355187+ludwigdn@users.noreply.github.com>
2025-06-07 15:51:38 +0200

    make format on save great again

--- nvim/lua/user/core/autocmds.lua
+++ nvim/lua/user/core/autocmds.lua
@@ -4,21 +4,6 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   command = [[%s/\s\+$//e]],
 })
 
--- Auto indent at save and return to last cursor position
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  callback = function()
-    -- Save current cursor position
-    local pos = vim.api.nvim_win_get_cursor(0)
-    -- Ident file(s)
-    vim.cmd(":execute 'normal! ggVG='")
-    -- Restore cursor position
-    vim.api.nvim_win_set_cursor(0, pos)
-    -- Center buffer on cursor
-    vim.cmd(":execute 'normal! zz'")
-  end
-})
-
 -- Auto wrap on markdown files
 local group = vim.api.nvim_create_augroup("Markdown Wrap Settings", { clear = true })
 vim.api.nvim_create_autocmd("BufEnter", {
--- nvim/lua/user/plugins/lsp/null-ls.lua
+++ nvim/lua/user/plugins/lsp/null-ls.lua
@@ -1,13 +1,48 @@
 local null_ls_status, null_ls = pcall(require, "null-ls")
 if not null_ls_status then
-  return
+	return
 end
+
+local formatting = null_ls.builtins.formatting
+local completion = null_ls.builtins.completion
+
+-- to setup format on save
+local augroup = vim.api.nvim_create_augroup("LspAutoFormatting", {})
 
 -- configure null_ls
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.completion.spell,
-    --require("none-ls.diagnostics.eslint"),
-  },
+	sources = {
+		completion.spell,
+		formatting.stylua,
+		formatting.prettier.with({
+			condition = function(utils)
+				return utils.root_has_file({ ".prettierrc" })
+			end,
+		}),
+		require("none-ls.diagnostics.eslint_d").with({
+			condition = function(utils)
+				return utils.root_has_file({ ".eslintrc.json", ".eslintrc.js", ".eslintrc.cjs" })
+			end,
+		}),
+	},
+
+	-- configure format on save
+	on_attach = function(current_client, bufnr)
+		if current_client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						filter = function(client)
+							--  only use null-ls for formatting instead of lsp server
+							return client.name == "null-ls"
+						end,
+						bufnr = bufnr,
+					})
+				end,
+			})
+		end
+	end,
 })
