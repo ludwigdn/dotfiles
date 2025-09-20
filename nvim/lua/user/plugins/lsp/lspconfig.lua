37cb123c5e16b9bfb2c445c06d45d43b940b235c
ludwigdn <11355187+ludwigdn@users.noreply.github.com>
2025-09-20 13:29:58 +0200

    nvim: lspconfig, use new way to enable configs

--- nvim/lua/user/plugins/lsp/lspconfig.lua
+++ nvim/lua/user/plugins/lsp/lspconfig.lua
@@ -1,28 +1,22 @@
-local lspconfig_status, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status then
-  return
-end
-
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
-  return
+	return
 end
 
 local navic_setup, navic = pcall(require, "nvim-navic")
 if not navic_setup then
-  return
+	return
 end
 
 navic.setup({
-  icons = {},
-  highlight = true,
+	icons = {},
+	highlight = true,
 })
-
 
 -- Lspsaga show_[...]_diagnostics now only works if severity_sort is truthty
 -- https://github.com/nvimdev/lspsaga.nvim/issues/1520
 vim.diagnostic.config({
-  severity_sort = true,
+	severity_sort = true,
 })
 
 -- Hover documentation
@@ -34,9 +28,9 @@ vim.keymap.set("n", "gi", ":lua vim.lsp.buf.implementation()<CR>") -- go to impl
 vim.keymap.set("n", "<leader>o", ":LSoutlineToggle<CR>") -- see outline on right hand side
 
 local on_attach = function(client, bufnr)
-  if client.server_capabilities.documentSymbolProvider then
-    navic.attach(client, bufnr)
-  end
+	if client.server_capabilities.documentSymbolProvider then
+		navic.attach(client, bufnr)
+	end
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
@@ -44,80 +38,80 @@ local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- Change the Diagnostic symbols in the sign column (gutter)
 vim.diagnostic.config({
-  virtual_lines = { current_line = true },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = " ",
-      [vim.diagnostic.severity.WARN] = " ",
-      [vim.diagnostic.severity.HINT] = " ",
-      [vim.diagnostic.severity.INFO] = " ",
-    },
-    linehl = {
-      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-    },
-    numhl = {
-      [vim.diagnostic.severity.WARN] = "WarningMsg",
-    },
-  },
+	virtual_lines = { current_line = true },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+		},
+		linehl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+		},
+		numhl = {
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+		},
+	},
 })
 
 -- configure html server
-lspconfig["html"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+vim.lsp.config("html", {
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 -- configure vue server
-lspconfig["vuels"].setup({
-  init_options = {
-    vue = {
-      hybridMode = false,
-    },
-  },
+vim.lsp.config("vuels", {
+	init_options = {
+		vue = {
+			hybridMode = false,
+		},
+	},
 })
 
 -- configure css server
-lspconfig["cssls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+vim.lsp.config("cssls", {
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 -- configure tailwindcss server
-lspconfig["tailwindcss"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+vim.lsp.config("tailwindcss", {
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 -- configure emmet language server
-lspconfig["emmet_ls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+vim.lsp.config("emmet_ls", {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
 })
 
 -- configure lua server (with special settings)
-lspconfig["lua_ls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  settings = { -- custom settings for lua
-    Lua = {
-      -- make the language server recognize "vim" global
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        -- make language server aware of runtime files
-        library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.stdpath("config") .. "/lua"] = true,
-        },
-      },
-    },
-  },
+vim.lsp.config("lua_ls", {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = { -- custom settings for lua
+		Lua = {
+			-- make the language server recognize "vim" global
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				-- make language server aware of runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
 })
 
 -- configure omnisharp server
-lspconfig["omnisharp"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+vim.lsp.config("omnisharp", {
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
